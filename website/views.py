713f48cb4bf99112bac00132143cf5f02aa571bb
Tatsuhiro <t.maru423@gmail.com>
2020-11-10 10:27:02 +0900

    change1

---
+++ website/views.py
@@ -0,0 +1,29 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name="index.html"
+
+    def get_context_data(self):
+        ctxt= super().get_context_data()
+        ctxt["username"] = "Tatsu"
+        return ctxt
+
+class AboutView(TemplateView):
+    template_name="about.html"
+
+    def get_context_data(self):
+        ctxt= super().get_context_data()
+        ctxt["num_services"] = 12345678
+        ctxt["skills"]=[
+            "Python",
+            "Javascript",
+            "C++"
+        ]
+        return ctxt
+
+        
+
+
+
+
+
